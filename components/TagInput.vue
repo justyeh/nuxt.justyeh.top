ac3a9430bca9a786b084e739dff12aa30f837f8b
justyeh <justyeh@163.com>
2017-06-22 18:39:07 +0800

    创建了一些组件：formgroup，图片上传，tag编辑，markdown

---
+++ components/TagInput.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="tags">
+            <label v-for="tag in tags">{{tag}}</label>
+        </div>
+        <input type="text" @keyup.enter.stop.prevent="handleEnter" v-model="tag">
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                tag:''
+            }
+        },
+        props:{
+            tags:{
+                type:Array
+            }
+        },
+        methods:{
+            handleEnter(){
+                if(this.tags.indexOf(this.tag) > -1){
+                    alert("请不要添加重复的标签");
+                    return;
+                }
+                this.$emit('addTag',this.tag);
+                this.tag = ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.tag-input {
+    margin: 10px 0;
+    display: flex;
+}
+label{
+    display: inline-block;
+    height: 26px;
+    line-height: 26px;
+    padding: 0 10px;
+    background: rgba(100, 100, 100, 0.12);
+    border-radius: 2px;
+    color: rgba(0, 0, 0, .54);
+    margin: 5px 10px 5px 0;
+}
+input {
+    height: 36px;
+    border: 1px solid #ccc;
+    width: 100%;
+}
+
+input {
+    text-indent: 15px;
+}
+
+input:focus {
+    border-color: rgb(51, 204, 250);
+}
+</style>
---
+++ components/YehMarkdown.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="markdown">
+        <div class="preview" @click="preview = !preview">{{preview ? '预览' : '编辑'}}</div>
+        <textarea placeholder="正文" v-model="markdown">{{markdown}}</textarea>
+        <vue-markdown class="yeh-markdown-theme" :source="markdown" v-show="preview"></vue-markdown>
+    </div>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown'
+
+export default {
+    props: ['markdown'],
+    components:{
+        VueMarkdown
+    },
+    data() {
+        return {
+            preview: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+.markdown {
+    position: relative;
+    height: 500px;
+    width: 100%;
+}
+
+.markdown .preview {
+    width: 40px;
+    height: 30px;
+    line-height: 30px;
+    box-shadow: 0 0 5px rgba(100, 100, 100, 0.4);
+    background: rgba(100, 100, 100, 0.2);
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    z-index: 100;
+    cursor: pointer;
+    border-radius: 4px;
+    text-align: center;
+    color: #1b8afa;
+}
+
+.markdown textarea {
+    padding: 5px 15px;
+    display: block;
+    height: 100%;
+    width: 100%;
+    border: 1px solid #ddd;
+    resize: none;
+}
+
+.markdown textarea:focus {
+    border-color: rgb(51, 204, 250);
+}
+
+.markdown .yeh-markdown-theme {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    z-index: 99;
+    overflow-y: auto;
+    background: #eee;
+    transition: width ease cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+
+.markdown>div.show {
+    width: 100%;
+}
+</style>
