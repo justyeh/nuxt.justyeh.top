4be20c1e8de5d59193495ae53a7ff3917a7e486d
justyeh <justyeh@163.com>
2017-06-22 18:37:34 +0800

    创建后台header和sidemenu

---
+++ components/admin/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <nuxt-link to="/" class="nav-left">
+      <img src="../../assets/img/head.jpg" />
+      <span>JustYeh的前端博客</span>
+    </nuxt-link>
+    <nuxt-link to="">退出</nuxt-link>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background: #1b8afa;
+  position: fixed;
+  left: 0;
+  right: 0;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+  font-size: 18px;
+  color: #fff;
+}
+
+header .nav-left {
+  display: flex;
+  align-items: center;
+}
+
+header .nav-left img {
+  width: 40px;
+  border-radius: 100%;
+  margin-right: 20px;
+}
+
+header a {
+  color: rgba(250, 250, 250, 0.8);
+}
+</style>
---
+++ components/admin/SideMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="side-menu">
+    <ul>
+      <li>
+        <nuxt-link to="admin/posts">博客管理</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="admin/tags">标签管理</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.side-menu {
+  position: fixed;
+  top: 80px;
+  bottom: 0;
+  left: 0;
+  width: 150px;
+  box-shadow: 0 5px 5px rgba(100, 100, 100, 0.5);
+  background: #263238;
+  z-index: 99;
+}
+
+.side-menu a {
+  display: block;
+  height: 60px;
+  line-height: 60px;
+  color: #fff;
+  text-align: center;
+  border-bottom: 1px solid rgba(100, 100, 100, 0.3)
+}
+
+.side-menu a:hover {
+  background: #263238;
+}
+</style>
