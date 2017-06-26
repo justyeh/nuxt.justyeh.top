33f1d5133e07f091209d834fb01e57369a0d8264
justyeh <justyeh@163.com>
2017-06-26 17:42:12 +0800

    完善footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,45 @@
 <template>
   <footer>
-  
+    <div>
+      <p>参考网址：</p>
+      <div class="links">
+        <a href="https://cnodejs.org/">CNode技术社区</a>
+        <a href="https://cn.vuejs.org/">Vue.js官网</a>
+        <a href="https://zh.nuxtjs.org/">Nuxt.js官网</a>
+        <a href="http://www.expressjs.com.cn/">Express.js官网</a>
+      </div>
+    </div>
+    <div>
+      <p>友情链接：</p>
+      <div class="links">
+        <a href="http://www.lanqiao.org/">蓝桥官网</a>
+        <a href="http://www.javabb.cn/">秦豹的个人网站</a>
+      </div>
+    </div>
   </footer>
 </template>
 
 <style scoped>
 footer {
   background: rgb(38, 50, 56);
-  padding: 150px 0;
+  padding: 50px 100px;
+  color: #fff;
+}
+
+div {
+  padding-bottom: 20px;
+}
+
+footer div:last-child {
+  padding-bottom: 0;
+}
+
+p {
+  font-size: 16px;
+  padding-bottom: 10px;
+}
+
+a {
+  margin-right: 20px;
 }
 </style>
