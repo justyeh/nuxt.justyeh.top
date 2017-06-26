005b45666755448cc80d6ac20e158ac8da9bedb1
justyeh <justyeh@163.com>
2017-06-26 10:56:09 +0800

    新增一个分页组件

---
+++ components/VuePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <nuxt to="">&larr;上一页</nuxt>
+      <nuxt to="">下一页&rarr;</nuxt>
+  </div>
+</template>
+
+<script>
+    export default {
+        props:['total','page']
+    }
+</script>
+
+<style scoped>
+div{
+    display: flex;
+    justify-content: space-between;
+}
+div a{
+    display: inline-block;
+    width: 100px;
+    padding: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+}
+div a:not(.disable):hover{
+    border-color: rgb(51, 204, 250);
+}
+</style>
