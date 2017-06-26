b3e8d43a6e8a2c6b4e10ccdff0b9757286a56b5f
justyeh <justyeh@163.com>
2017-06-26 17:44:01 +0800

    测试tools页面

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="page">
-        <vue-page total="10" page="5" path="?page="></vue-page>
+        <vue-page total='10' page='1' path='?page=' ></vue-page>
+        <material-design-color></material-design-color>
     </div>
 </template>
 
 <script>
 import VuePage from '../../components/VuePage'
+import MaterialDesignColor from '../../components/tools/MaterialDesignColor'
 export default {
     head() {
         return {
@@ -13,7 +15,8 @@ export default {
         }
     },
     components: {
-        VuePage
+        VuePage,
+        MaterialDesignColor
     }
 }
 </script>
