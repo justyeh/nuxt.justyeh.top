12528e8fd8ef6b16159587106c3186dc02d8ef19
justyeh <justyeh@163.com>
2017-06-27 19:22:47 +0800

    添加一个工具方法

--- util/assist.js
+++ util/assist.js
@@ -13,6 +13,19 @@ function formatDate(timestamp) {
     return where;
 }*/
 
+export function timeAgo (time) {
+  const between = Date.now() / 1000 - Number(time)
+
+  if (between < 3600) {
+    return pluralize(~~(between / 60), ' 分钟前')
+  } else if (between < 86400) {
+    return pluralize(~~(between / 3600), ' 小时前')
+  } else {
+    return pluralize(~~(between / 86400), ' 天前')
+  }
+}
+
+
 export {
     formatDate,
     objectToSQLWhrer
