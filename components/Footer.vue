3c9023ee8c60ffa325085b76fafe1ce68edd59a9
justyeh <justyeh@163.com>
2017-06-28 11:25:13 +0800

    修改站外链接为新窗口打开

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,17 +3,17 @@
     <div>
       <p>参考网址：</p>
       <div class="links">
-        <a href="https://cnodejs.org/">CNode技术社区</a>
-        <a href="https://cn.vuejs.org/">Vue.js官网</a>
-        <a href="https://zh.nuxtjs.org/">Nuxt.js官网</a>
-        <a href="http://www.expressjs.com.cn/">Express.js官网</a>
+        <a target="_blank" href="https://cnodejs.org/">CNode技术社区</a>
+        <a target="_blank" href="https://cn.vuejs.org/">Vue.js官网</a>
+        <a target="_blank" href="https://zh.nuxtjs.org/">Nuxt.js官网</a>
+        <a target="_blank" href="http://www.expressjs.com.cn/">Express.js官网</a>
       </div>
     </div>
     <div>
       <p>友情链接：</p>
       <div class="links">
-        <a href="http://www.lanqiao.org/">蓝桥官网</a>
-        <a href="http://www.javabb.cn/">秦豹的个人网站</a>
+        <a target="_blank" href="http://www.lanqiao.org/">蓝桥官网</a>
+        <a target="_blank" href="http://www.javabb.cn/">秦豹的个人网站</a>
       </div>
     </div>
   </footer>
