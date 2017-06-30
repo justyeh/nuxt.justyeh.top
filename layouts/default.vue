81307a4460390adf4dbde8e66c738fda7f1474c9
justyeh <justyeh@163.com>
2017-06-30 15:06:49 +0800

    优化页面布局

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app">
     <my-header></my-header>
     <nuxt></nuxt>
     <my-footer></my-footer>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="page">
+  <div class="page">
     <h1>关于这个网站</h1>
     <p>大家好，我叫叶文祥，一位前端码畜，喜欢跟潮流，也是为了不落伍，目前正在学习Vue.js和ES6，有时也会使用node.js，不过属于玩票性质！</p>
     <p>这个网站算是我的业余成果吧，使用的技术栈：Vue.js+Nuxt.js+Es6+Webpack+Mysql+Noyde.js+Express.js，我已将网站开源到GitHub，项目地址
@@ -7,7 +7,7 @@
     <p>如果你也对前端感兴趣，欢迎和我交流，我的邮箱
       <a href="mailto:justyeh@163.com">justyeh@163.com</a>，有时也会做一些微不足道的开源工作，这是我的GitHub主页
       <a target="_blank" href="https://github.com/justyeh">https://github.com/justyeh</a>。</p>
-  </section>
+  </div>
 </template>
 
 <script>
