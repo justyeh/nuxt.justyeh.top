a66c896d54628f382485bbfb8b0ffdce7b8884a5
justyeh <justyeh@163.com>
2017-06-30 14:03:59 +0800

    优化DB工具类，避免sql错误导致程序崩溃

--- server/db/DBUtil.js
+++ server/db/DBUtil.js
@@ -3,24 +3,30 @@ let serverCfg = require('../../util/mysql.config')
 
 let pool = mysql.createPool(serverCfg.db);
 
-let query = function(sql,params,callback){
-    pool.getConnection( (err,connection) => {
-        if (err) {
-            callback(true);
-            return;
-        }
-        //链接
-        connection.query(sql,params, (err,result) => {
-            //释放链接
-            connection.release();
-            if(err){
-                console.error('db error:' + err)
+let query = function (sql, params, callback) {
+    try {
+        pool.getConnection((err, connection) => {
+            if (err) {
                 callback(true);
                 return;
             }
-            callback(false,result);
+            //链接
+            connection.query(sql, params, (err, result) => {
+                //释放链接
+                connection.release();
+                if (err) {
+                    console.error('db error:' + err)
+                    callback(true);
+                    return;
+                }
+                callback(false, result);
+            });
         });
-    });
+    } catch (error) {
+        console.log(true)
+        callback(true);
+        return false;
+    }
 }
 
 module.exports.query = query;
