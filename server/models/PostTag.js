5133bcba59b2cfc97ef183daf5a5a4e4947add24
justyeh <justyeh@163.com>
2017-06-30 14:01:23 +0800

    将post和tag的关联查询提取到一个Model

---
+++ server/models/PostTag.js
@@ -0,0 +1,28 @@
+
+let db = require('../db/DBUtil');
+
+class PostTag {
+
+    //根据PostId获取Tag列表
+    tagsByPostId(postId, callback) {
+        let sql = "SELECT tags.id,tags.name FROM posts,tags,post_tags WHERE posts.id = post_tags.post_id AND tags.id = post_tags.tag_id AND posts.id = ?";
+        db.query(sql, [postId], (err, result) => {
+            if (err) {
+                return;
+            }
+            callback(false, result);
+        });
+    }
+    //根据TagId获取Post列表
+    postsByTagId(tagId,callback){
+        let sql = 'SELECT posts.id,posts.title,posts.image,posts.meta_description FROM posts,post_tags WHERE posts.id = post_tags.post_id AND posts.status = "published" AND post_tags.tag_id = ?';
+        db.query(sql, [tagId], (err, result) => {
+            if (err) {
+                return;
+            }
+            callback(false, result);
+        });
+    }
+}
+
+module.exports = PostTag;
