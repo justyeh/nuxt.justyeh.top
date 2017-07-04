eb1fe121167c26fcfd0beba011daf6b0f917196a
justyeh <justyeh@163.com>
2017-07-04 19:16:41 +0800

    增加页面标题组件

---
+++ components/admin/PageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrap">
+        <p>{{title}}</p>
+        <slot name="handle"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style scoped>
+.wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: solid 1px #ddd;
+    padding: 20px 0
+}
+
+p {
+    font-size: 18px;
+}
+</style>
