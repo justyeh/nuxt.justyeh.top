5306059001075b9fb22ed154fc754eaa3d1cfd2e
justyeh <justyeh@163.com>
2017-07-04 19:21:44 +0800

    Tag管理页面

---
+++ pages/admin/tag/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tags">
+        tag management
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'admin'
+}
+</script>
+
+<style scoped>
+.tags {
+    display: flex;
+    height: 100%
+}
+</style>
