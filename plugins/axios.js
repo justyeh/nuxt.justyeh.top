8433ab5424c61be5197d94ab2ecd82aaaf152dee
justyeh <justyeh@163.com>
2017-07-06 11:37:51 +0800

    添加axios拦截器示例

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,33 @@
 import * as axios from 'axios'
 
-let options = {}
+let options = {
+}
 // The server-side needs a full url to works
 if (process.SERVER_BUILD) {
   options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`
 }
 
-export default axios.create(options)
+let $http = axios.create(options)
+
+// 拦截request
+/*$http.interceptors.request.use(
+    config => {
+        if (store.state.token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
+            config.headers.Authorization = `token ${store.state.token}`;
+        }
+        return config;
+    },
+    err => {
+        return Promise.reject(err);
+});*/
+
+// 拦截response
+/*$http.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+*/
+
+export default $http
