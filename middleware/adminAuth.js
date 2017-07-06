ea5ffeffdb7a69b30e4b10ca4036d8446bfbc7fa
justyeh <justyeh@163.com>
2017-07-06 11:39:32 +0800

    登陆中间件

---
+++ middleware/adminAuth.js
@@ -0,0 +1,21 @@
+import { isLogin } from '../util/assist'
+const needAuth = require('../util/api.config').needAuth
+
+export default function ({ isClient, isServer, route, req, redirect }) {
+    //在服务端判读是否需要登陆(如果直接输地址，在客户端是判断不到的)
+    if (isServer) {
+        let cookies = req.cookies
+        let path = req.originalUrl;
+
+        if (path.indexOf('admin') > 0 && !cookies.token) {
+            redirect('/login')
+        }
+    }
+    //在客户端判读是否需要登陆
+    if (isClient) {
+        console.log(route.path.indexOf('admin'))
+        if (!isLogin() && route.path.indexOf('admin') > 0) {
+            redirect('login')
+        }
+    }
+}
