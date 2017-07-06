cf16640465129aaac37584b4a02e66e327bbd4df
justyeh <justyeh@163.com>
2017-07-06 11:41:08 +0800

    添加应该登陆拦截的接口列表

--- util/api.config.js
+++ util/api.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    pageSize: 20
+    pageSize: 20,
+    needAuth: [
+        '/api/post/add',
+        '/api/post/update',
+        '/api/post/tag/add',
+        '/api/post/tag/del',
+        '/api/tag/add',
+    ]
 }
