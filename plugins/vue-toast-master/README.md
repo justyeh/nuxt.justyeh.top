30daded05657f1db8c3bace6176e2f1ad0ff6085
justyeh <justyeh@163.com>
2017-07-19 09:44:08 +0800

    插件示例

---
+++ plugins/vue-toast-master/README.md
@@ -0,0 +1,71 @@
+# vue2-toast
+A mobile toast plugin for vue2.(can't work in Nuxt.js/SSR)
+
+<p>
+  <a href="https://www.npmjs.com/package/vue2-toast"><img src="https://img.shields.io/npm/dm/vue2-toast.svg" alt="Downloads"></a>
+  <a href="https://www.npmjs.com/package/vue2-toast"><img src="https://img.shields.io/npm/v/vue2-toast.svg" alt="Version"></a>
+  <br>
+</p>
+
+## Usage
+Install:
+
+```
+npm install vue2-toast -S
+```
+Import:
+
+```javascript
+import 'vue2-toast/lib/toast.css';
+import Toast from 'vue2-toast';
+Vue.use(Toast);
+```
+
+Use in component:
+
+```javascript
+<template>
+    <div id="app">
+        <button @click="openTop()">top</button>
+        <button @click="openCenter()">center</button>
+        <button @click="openBottom()">bottom</button>
+		<button @click="openLoading()">loading</button>
+    </div>
+</template>
+export default {
+    methods:{
+        openTop(){
+            this.$toast.top('top');
+        },
+        openCenter(){
+            this.$toast.center('center');
+        },
+        openBottom(){
+            this.$toast('bottom');  // or this.$toast.bottom('bottom'); 
+        },
+        openLoading(){
+            this.$loading('loading...');
+			let self = this;
+	        setTimeout(function () {
+	          self.closeLoading()
+	        }, 2000)
+        },
+        closeLoading(){
+            this.$loading.close();
+        }
+    }
+}
+```
+
+## options
+
+    Vue.use(Toast, [options])
+
+- defaultType : position of Toast. | default: 'bottom' | possible 'top, center,bottom'
+- duration : default 2500ms
+
+## source code
+download in [Github](https://github.com/lin-xin/vue-toast).
+
+## demo
+![image](https://raw.githubusercontent.com/lin-xin/vue-toast/master/screenshots/1.gif)
---
+++ plugins/vue-toast-master/index.js
@@ -0,0 +1,79 @@
+/**
+ * Updated by linxin on 2017/7/5.
+ */
+var Toast = {};
+var showToast = false,      // 存储toast显示状态
+    showLoad = false,       // 存储loading显示状态
+    loadNode = null;        // 存储loading节点元素
+
+Toast.install = function (Vue, options) {
+    var opt = {
+        defaultType:'bottom',
+        duration:'2500'
+    };
+    for(var property in options){
+        opt[property] = options[property];
+    }
+    Vue.prototype.$toast = function(tips,type){
+
+        var curType = type ? type : opt.defaultType;
+
+        if(showToast){
+            // 如果toast还在，则不再执行
+            return;
+        }
+        var toastTpl = Vue.extend({
+            data: function(){
+                return {
+                    show: showToast
+                }
+            },
+            template: '<div v-show="show" class="lx-toast lx-toast-'+ curType +'">' + tips + '</div>'
+        });
+        var vm = new toastTpl()
+        var tpl = vm.$mount().$el;
+
+        document.body.appendChild(tpl);
+        vm.show = showToast = true;
+
+        setTimeout(function () {
+            vm.show = showToast = false;
+        }, opt.duration)
+    };
+    ['bottom', 'center', 'top'].forEach(function(type) {
+        Vue.prototype.$toast[type] = function(tips) {
+            return Vue.prototype.$toast(tips,type)
+        }
+    });
+
+    Vue.prototype.$loading = function(tips,type) {
+        if(type == 'close'){
+            loadNode.show = showLoad = false;
+        }else{
+            if(showLoad){
+                // 如果loading还在，则不再执行
+                return;
+            }
+            var loadTpl = Vue.extend({
+                data: function(){
+                    return {
+                        show: showLoad
+                    }
+                },
+                template: '<div v-show="show" class="lx-load-mark"><div class="lx-load-box"><div class="lx-loading"><div class="loading_leaf loading_leaf_0"></div><div class="loading_leaf loading_leaf_1"></div><div class="loading_leaf loading_leaf_2"></div><div class="loading_leaf loading_leaf_3"></div><div class="loading_leaf loading_leaf_4"></div><div class="loading_leaf loading_leaf_5"></div><div class="loading_leaf loading_leaf_6"></div><div class="loading_leaf loading_leaf_7"></div><div class="loading_leaf loading_leaf_8"></div><div class="loading_leaf loading_leaf_9"></div><div class="loading_leaf loading_leaf_10"></div><div class="loading_leaf loading_leaf_11"></div></div><div class="lx-load-content">'+tips+'</div></div></div>'
+            });
+            loadNode = new loadTpl();
+            var tpl = loadNode.$mount().$el;
+
+            document.body.appendChild(tpl);
+            loadNode.show = showLoad = true;
+        }
+    };
+
+    ['open', 'close'].forEach(function(type) {
+        Vue.prototype.$loading[type] = function(tips) {
+            return Vue.prototype.$loading(tips,type)
+        }
+    });
+}
+module.exports = Toast;
